8e4e909055a6ece11a737e63ef1dc1928cff8d8c
marshget <marshbranded5@gmail.com>
2024-10-23 18:45:03 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+    return (
+        <div>
+            <h1>Selamat datang di Proyek Vercel Anda!</h1>
+            <p>API untuk validasi ID dapat diakses di <code>/api/allowed_ids</code></p>
+        </div>
+    );
+      }
