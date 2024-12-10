ae119382ed79a38082368c3cda6ffa21c548acec
marshget <marshbranded5@gmail.com>
2024-12-10 14:39:17 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-// pages/index.js
-export default function Home() {
-    return (
-        <div>
-            <h1>Selamat datang di Proyek Vercel Anda!</h1>
-            <p>API untuk validasi ID dapat diakses di <code>/api/allowed_ids</code></p>
-        </div>
-    );
-      }
